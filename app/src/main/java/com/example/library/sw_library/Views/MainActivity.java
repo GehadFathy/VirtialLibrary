b9c5dc4c77dadf7d4e62c7a97e176a5500cb7f4c
GehadFathy <gehadfathy99@gmail.com>
2018-04-17 23:09:21 +0200

    connected to API,, cache books in table but still errors in corner cases

--- app/src/main/java/com/example/library/sw_library/Views/MainActivity.java
+++ app/src/main/java/com/example/library/sw_library/Views/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.library.sw_library.Database.DBHelper;
+import com.example.library.sw_library.Network.GoogleApiRequest;
 import com.example.library.sw_library.R;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DBHelper mydb = new DBHelper(this);
+       DBHelper mydb = new DBHelper(this);
         mydb.fillCategory();
+        mydb.getCategories();
+        try {
+            mydb.fillBooksFromAPI();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         mydb.getBooks(1);
         mydb.close();
 
