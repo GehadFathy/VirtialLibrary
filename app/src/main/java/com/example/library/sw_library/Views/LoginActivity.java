1d21b63f9d193ac5994abbab48976a6cb419e4d1
asmaa <sh.asmosama@hotmail.com>
2018-04-27 08:14:47 +0200

    junk

--- app/src/main/java/com/example/library/sw_library/Views/LoginActivity.java
+++ app/src/main/java/com/example/library/sw_library/Views/LoginActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.library.sw_library.Database.DBHelper;
 import com.example.library.sw_library.R;
 
 /**
@@ -15,15 +18,23 @@ import com.example.library.sw_library.R;
 public class LoginActivity extends AppCompatActivity  {
 
     private Button loginButton;
-    private AutoCompleteTextView emailView;
+    private EditText emailView;
+    private DBHelper dbManager;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         loginButton = findViewById(R.id.email_sign_in_button);
-//        loginButton.setOnClickListener();
+//        loginButton.setOnClickListener(new Button.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                String mail = (R.id.email);
+//            }
+//        });
     }
 
 }
